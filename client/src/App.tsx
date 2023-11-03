3307bb24dfe02a4466d1d9125149242bfd1ffb37
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-11-03 00:07:36 -0500

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Nasa from "./components/Nasa";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Nasa />
+      </div>
+    </div>
+  );
+}
+
+export default App;
