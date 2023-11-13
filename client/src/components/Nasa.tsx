7fc3b182002bae5f95c761dd87613263e5d2635e
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-11-12 22:56:45 -0600

    add pagination

--- client/src/components/Nasa.tsx
+++ client/src/components/Nasa.tsx
@@ -1,41 +1,59 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import RoverData from "../interfaces/Roverdata";
 
 import "./Nasa.css";
-import RoverData from "../interfaces/Roverdata";
 
 const Nasa: React.FC = () => {
   const [data, setData] = useState<RoverData[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async (page: number) => {
+    try {
+      setLoading(true);
+
+      const { data: responseData } = await axios.get<RoverData[]>(
+        `http://localhost:4000/nasa?page=${page}`
+      );
+
+      const transformedData = responseData.map((item) => ({
+        id: item.id,
+        img_src: item.img_src,
+        camera: { name: item.camera.name },
+      }));
+
+      setData((prevData) => [...prevData, ...transformedData]);
+    } catch (error) {
+      console.error("Error fetching data from server:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const getDataFromServer = async () => {
-      try {
-        const { data } = await axios.get<RoverData[]>(
-          "http://localhost:4000/nasa"
-        );
-        console.log("data", data);
-        const transformedData = data.map(item => ({
-          id: item.id,
-          img_src: item.img_src,
-          camera: { name: item.camera.name }
-        }));
-        setData(transformedData);
-      } catch (error) {
-        console.error("Error fetching data from server:", error);
-      }
-    };
-    getDataFromServer();
-  }, []);
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  const handleLoadMore = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+    fetchData(currentPage + 1);
+  };
 
   return (
     <div>
-      <div>
-        {data.map(({ id, camera, img_src }) => (
-          <ul key={id} className="container">
-            <h4>Camera name: {camera.name}</h4>
-            <img className="box" src={img_src} />
-          </ul>
-        ))}
+      <div className="main-container">
+        <div>
+          {data.map(({ id, camera, img_src }) => (
+            <ul key={id} className="container">
+              <h4>Camera name: {camera.name}</h4>
+              <img className="box" src={img_src} alt={`Mars Rover - ${id}`} />
+            </ul>
+          ))}
+        </div>
+        <button onClick={handleLoadMore} disabled={loading}>
+          {loading ? "Loading..." : "Load More Photos"}
+        </button>
       </div>
     </div>
   );
