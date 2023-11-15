5d3b369041a41ac66ec7997e6af8cd500dbd23a0
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-11-14 18:58:39 -0600

    fix the issue with filter

--- server.ts
+++ server.ts
@@ -13,21 +13,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 dotenv.config();
 
 const apiConfig = {
-  base: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/",
+  base: process.env.API_BASE,
   key: process.env.API_KEY,
 };
 
 app.get("/nasa", async (req, res) => {
   try {
     const { page = 1 } = req.query;
-    const perPage = 5; 
+    const perPage = 5;
+    // console.log("page", page)
     const { data } = await axios.get(
-      `${apiConfig.base}photos?sol=1000&api_key=${apiConfig.key}&page=${page}&per_page=${perPage}`
+      `${apiConfig.base}photos?sol=1000&api_key=${apiConfig.key}&per_page=${perPage}&page=${page}`
     );
     res.send(data.photos);
   } catch (error) {
-    console.error(`Error fetching data: ${error}`);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error(`error in /nasa route in get request ${error}`);
+    res
+      .status(500)
+      .json({ error: `error in /nasa route in get request ${error}` });
   }
 });
 
