4355da840cf86d4299d0bdb5ea9b220bf9b4a27a
Duygu Tuncer Mullin <duygu.tuncer001@gmail.com>
2023-11-14 18:59:06 -0600

    fix the issue with filter

--- client/src/components/Nasa.tsx
+++ client/src/components/Nasa.tsx
@@ -1,101 +1,90 @@
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import RoverData from "../interfaces/Roverdata";
 
 import "./Nasa.css";
+import RoverData from "../interfaces/Roverdata";
 
 const Nasa: React.FC = () => {
   const [data, setData] = useState<RoverData[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [filter, setFilter] = useState("");
-  const [filteredData, setFilteredData] = useState<RoverData[]>([]);
+  const [filteredItems, setFilteredItems] = useState<RoverData[]>([]);
 
-  const fetchData = async (page: number) => {
+  const getDataFromServer = async (page: number) => {
     try {
       setLoading(true);
-
-      const { data: responseData } = await axios.get<RoverData[]>(
+      const { data } = await axios.get<RoverData[]>(
         `http://localhost:4000/nasa?page=${page}`
       );
-
-      const transformedData = responseData.map((item) => ({
+      const transformedData = data.map((item) => ({
         id: item.id,
         img_src: item.img_src,
         camera: { name: item.camera.name },
         rover: { cameras: item.rover.cameras },
       }));
-
       setData((prevData) => [...prevData, ...transformedData]);
-
-      // if (filter) {
-      //   const filteredResults = transformedData.filter((item) =>
-      //     item.camera.name.toLowerCase().includes(filter.toLowerCase())
-      //   );
-      //   setFilteredData((prevFilteredData) => [
-      //     ...prevFilteredData,
-      //     ...filteredResults,
-      //   ]);
-      // }
-    } catch (error) {
-      console.error("Error fetching data from server:", error);
-    } finally {
       setLoading(false);
+    } catch (error) {
+      console.error(`error fetching data in get request ${error}`);
     }
   };
 
   useEffect(() => {
-    fetchData(currentPage);
+    getDataFromServer(currentPage);
   }, [currentPage]);
 
-  const handleLoadMore = () => {
+  const loadMoreHandler = () => {
     setCurrentPage((prevPage) => prevPage + 1);
-    fetchData(currentPage + 1);
+    // getDataFromServer(currentPage + 1);
   };
 
   const inputFieldHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilter(e.target.value);
   };
 
-  const filterClickHandler = () => {
-    const filteredResults = data.filter((item) =>
-      item.camera.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    setFilteredData(filteredResults);
-    setFilter("");
+  const seachInputClickHandler = () => {
+    const filteredData = data.filter((item) => {
+      return item.camera.name.toLowerCase().includes(filter.toLowerCase());
+    });
+    setFilteredItems(filteredData);
   };
 
   return (
     <div>
       <div className="main-container">
         <section>
+          <h2>Seach Photos By Camera Names</h2>
           <input
             type="text"
-            placeholder="find your rover photo by camera name"
+            placeholder="search by camera name"
             value={filter}
             onChange={inputFieldHandler}
           ></input>
-          <button onClick={filterClickHandler}>Seach</button>
-        </section>
-        <div>
-          {filteredData.map(({ id, camera, img_src, rover }) => (
-            <ul key={id} className="container">
-              <h4>Camera name: {camera.name}</h4>
-              <img className="box" src={img_src} alt={`Mars Rover - ${id}`} />
-              <div>
-                <h5>Cameras:</h5>
-                <ul>
-                  {rover.cameras.map((camera) => (
-                    <li key={camera.name}>{camera.full_name}</li>
-                  ))}
-                </ul>
+          <button onClick={seachInputClickHandler}>Search</button>
+          <div className="filter-container">
+            {filteredItems.map(({ id, camera, img_src, rover }) => (
+              <div key={id}>
+                <h3>{camera.name}</h3>
+                <img className="box" src={img_src} alt="hey" />
               </div>
-            </ul>
-          ))}
-        </div>
-        <button onClick={handleLoadMore} disabled={loading}>
-          {loading ? "Loading..." : "Load More Photos"}
-        </button>
+            ))}
+          </div>
+        </section>
+        {data.map(({ id, camera, img_src, rover }) => (
+          <li key={id} className="container">
+            <h3>{camera.name}</h3>
+            <img className="box" src={img_src} alt="hey" />
+            {rover.cameras.map(({ name, full_name }) => (
+              <li key={name}>{full_name}</li>
+            ))}
+          </li>
+        ))}
+        <section>
+          <button onClick={loadMoreHandler} disabled={loading}>
+            {loading ? "Loading" : "Load More"}
+          </button>
+        </section>
       </div>
     </div>
   );
--- client/src/interfaces/Roverdata.ts
+++ client/src/interfaces/Roverdata.ts
@@ -1,8 +1,8 @@
 interface RoverData {
   id: number;
-  sol?: number;
   camera: {
     name: string;
+    full_name?: string;
   };
   img_src: string;
   earth_date?: string;
